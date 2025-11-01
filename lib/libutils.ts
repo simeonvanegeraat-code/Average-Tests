48122f9d31d97efee1ecb0a241c76dd49af326e5
simeonvanegeraat-code <simeon.van.egeraat@gmail.com>
2025-11-01 20:35:56 +0100

    1

---
+++ lib/libutils.ts
@@ -0,0 +1,6 @@
+// lib/utils.ts
+// Minimalistische className helper zonder externe deps.
+// Gebruik: cn("p-4", condition && "bg-black")
+export function cn(...classes: Array<string | false | null | undefined>) {
+  return classes.filter(Boolean).join(" ");
+}
--- lib/twMerge.ts
+++
@@ -1,5 +0,0 @@
-// Minimal no-op twMerge to avoid adding Tailwind in MVP.
-// Swap to 'tailwind-merge' if you add Tailwind later.
-export function twMerge(input: string) {
-  return input;
-}
