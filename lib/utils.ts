1d78aa69eaa9bf16df37ea8224b97194926f5927
simeonvanegeraat-code <simeon.van.egeraat@gmail.com>
2025-12-30 00:52:53 +0100

    1

--- lib/utils.ts
+++
@@ -1,6 +0,0 @@
-// lib/utils.ts
-// Minimalistische className helper zonder externe dependencies.
-// Gebruik: cn("p-4", condition && "bg-black")
-export function cn(...classes: Array<string | false | null | undefined>) {
-  return classes.filter(Boolean).join(" ");
-}
